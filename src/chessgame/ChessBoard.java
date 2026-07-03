85e236ef2ca8a4378ec810c2179c41421cb3fcff
Vitor007345 <vitordinelli2007@hotmail.com>
2026-07-03 00:57:53 -0300

    fix: fullmoves ficava negativo resolvido

--- src/chessgame/ChessBoard.java
+++ src/chessgame/ChessBoard.java
@@ -393,7 +393,7 @@ public class ChessBoard {
 			
 			Move lastMove = this.moves.getLast();
 			
-			if (!this.whiteToMove) { 
+			if (this.whiteToMove) { 
                 this.fullmoveNumber--;
             }
 			
